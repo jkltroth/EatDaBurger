66e06ba8228c5104d8d1f7d57469fb39bac06631
James Roth <jkltroth@gmail.com>
2018-01-08 22:05:27 -0500

    Added function to convert object key/value pairs to SQL syntax

--- config/orm.js
+++ config/orm.js
@@ -1,6 +1,29 @@
 // Import MySQL connection.
 var connection = require("../config/connection.js");
 
+// Helper function to convert object key/value pairs to SQL syntax
+function objToSql(object) {
+    var array = [];
+
+    // loop through the keys and push the key/value as a string int arr
+    for (var key in object) {
+        var value = object[key];
+        // check to skip hidden properties
+        if (Object.hasOwnProperty.call(object, key)) {
+            // if string with spaces, add quotations (Lana Del Grey => 'Lana Del Grey')
+            if (typeof value === "string" && value.indexOf(" ") >= 0) {
+                value = "'" + value + "'";
+            }
+            // e.g. {name: 'Lana Del Grey'} => ["name='Lana Del Grey'"]
+            // e.g. {sleepy: true} => ["sleepy=true"]
+            array.push(key + "=" + value);
+        }
+    }
+
+    // translate array of strings to a single comma-separated string
+    return array.toString();
+}
+
 // Object for all our SQL statement functions.
 var orm = {
     selectAll: function (table, cb) {
